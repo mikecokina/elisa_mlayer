0efd98cd5669f3969134fb2f22304a2e230d6828
mike <mikecokina@gmail.com>
2020-02-16 10:21:53 +0100

    detached - high density period - dry run

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ detached:
 
     PARMAS = {
         "system": {
-            "inclination": (30.0, 95.0, 5.0),
+            "inclination": (60.0, 95.0, 5.0),
             # reference: https://www.cambridge.org/core/journals/publications-of-the-astronomical-society-of-australia/article/catalogue-of-stellar-parameters-from-the-detached-doublelined-eclipsing-binaries-in-the-milky-way/678AD993366CA4506DD671527906FCBD/core-reader
-            "period": (7.5, 40.0, 5.0) # lower density (lower density is based on information that there is less light curves in such period range)
-            "period": (0.4, 7.5, 0.5) # higher density
+            "period": (3.0, 40.0, 5.0) # lower density (lower density is based on information that there is less light curves in such period range)
+            "period": (0.4, 3.0, 0.5) # higher density
         },
         "primary": {
             # reference: https://www.cambridge.org/core/journals/publications-of-the-astronomical-society-of-australia/article/catalogue-of-stellar-parameters-from-the-detached-doublelined-eclipsing-binaries-in-the-milky-way/678AD993366CA4506DD671527906FCBD/core-reader
             "mass": (0.2, 3.25, 0.25),
-            "surface_potential": (2.0, 6.0, 0.25),
-            "t_eff": (4000.0, 6500.0, 500.0)
+            "surface_potential": (2.0, 6.0, 0.5),
+            "t_eff": (4000.0, 15000.0, 1000.0)
         },
         "secondary": {
             "mass": (0.2, 3.25, 0.25),
-            "surface_potential": (2.0, 6.0, 0.25),
-            "t_eff": (4000.0, 6500.0, 500.0)
+            "surface_potential": (2.0, 6.0, 0.5),
+            "t_eff": (4000.0, 15000.0, 1000.0)
         }
     }
 
@@ -80,6 +80,22 @@ PARMAS = {
     }
 }
 
+PARMAS = {
+    "system": {
+        "inclination": (60.0, 95.0, 5.0),
+        "period": (0.4, 3.0, 0.5)  # higher density
+    },
+    "primary": {
+        "mass": (0.2, 3.25, 0.25),
+        "surface_potential": (2.0, 6.0, 0.5),
+        "t_eff": (4000.0, 15000.0, 1000.0)
+    },
+    "secondary": {
+        "mass": (0.2, 3.25, 0.25),
+        "surface_potential": (2.0, 6.0, 0.5),
+        "t_eff": (4000.0, 15000.0, 1000.0)
+    }
+}
 
 # PARMAS = {
 #     "system": {
@@ -108,14 +124,14 @@ PARMAS = {
 
 
 def main():
-    _cli = runner.Runner(morphology="over-contact",
-                         db_conf=DB_CONF,
-                         params=PARMAS,
-                         phases=PHASES,
-                         passband=PASSBAND,
-                         threshold=0.01)
+    _runner = runner.Runner(morphology="detached",
+                            db_conf=DB_CONF,
+                            params=PARMAS,
+                            phases=PHASES,
+                            passband=PASSBAND,
+                            threshold=0.01)
 
-    _cli.run()
+    _runner.run()
 
     # from elisa_mlayer.gen import io
     # from elisa_mlayer.gen import plot
